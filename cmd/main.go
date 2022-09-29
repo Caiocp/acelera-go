ed119a8b10050db9e5d339a0785719f5dd31ce28
Caio Carvalho <ccarvalho451@gmail.com>
2022-09-29 14:23:32 +0000

    adiciona consumer de mensagens

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"sync"
 
 	"github.com/caiocp/go-intensivo/internal/order/infra/database"
 	"github.com/caiocp/go-intensivo/internal/order/usecases"
+	"github.com/caiocp/go-intensivo/pkg/rabbitmq"
+	_ "github.com/go-sql-driver/mysql"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
+	maxWorkers := 3
+	wg := sync.WaitGroup{}
+	
 	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/orders")
 	if err != nil {
 		panic(err)
@@ -17,18 +26,42 @@ func main() {
 
 	repository := database.NewOrderRepository(db)
 
-	createUseCase := usecases.NewCalculateFinalPriceUseCase(repository)
+	createOrderUseCase := usecases.NewCalculateFinalPriceUseCase(repository)
 
-	input := usecases.CreateOrderInputDto{
-		ID:    "123",
-		Price: 100,
-		Tax:   10,
-	}
-
-	output, err := createUseCase.Execute(input)
+	ch, err := rabbitmq.OpenChannel()
 	if err != nil {
 		panic(err)
 	}
+	defer ch.Close()
 
-	println(output.FinalPrice)
+	out := make(chan amqp.Delivery)
+
+	go rabbitmq.Consume(ch, out)
+
+	wg.Add(maxWorkers)
+	for i := 0; i < maxWorkers; i++ {
+		defer wg.Done()
+		go worker(out, createOrderUseCase, i)
+	}
+	wg.Wait()
+}
+
+func worker(deliveryMessage <-chan amqp.Delivery, usecase *usecases.CalculateFinalPriceUseCase, worderId int) {
+	for message := range deliveryMessage {
+		var input usecases.CreateOrderInputDto
+		err := json.Unmarshal(message.Body, &input)
+		if err != nil {
+			fmt.Println("Error unmarshalling message: ", err)
+		}
+
+		input.Tax = 10.0
+
+		_, err = usecase.Execute(input)
+		if err != nil {
+			fmt.Println("Error unmarshalling message: ", err)
+		}
+
+		message.Ack(false)
+		fmt.Println("Worker ", worderId, " processed message: ", input.ID)
+	}
 }
